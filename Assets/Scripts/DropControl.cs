48a78e61ae2d13f19dc8b23e9ed65ed512627ee6
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2020-11-08 19:12:29 -0500

    Updated the chess scene

--- Assets/Scripts/DropControl.cs
+++ Assets/Scripts/DropControl.cs
@@ -20,9 +20,24 @@ public class DropControl : MonoBehaviour, IDropHandler
         Debug.Log(eventData.pointerDrag.name);
         if (eventData.pointerDrag.name == "Chessboard")
         {
+            disableChar("mom");
+            disableChar("dad");
+            disableChar("daughter");
+            disableChar("son");
             animationSlot.SetActive(true);
             animationSlot.GetComponent<Image>().sprite = chessGame;
         }
+        
+    }
+
+    void disableChar(string character)
+    {
+        if (character != "dad")
+        {
+            GameObject.Find(character).GetComponent<Image>().enabled = false;
+        }
+        string char_bubble = character + "_bubble";
+        GameObject.Find(char_bubble).GetComponent<Image>().enabled = false;
     }
 
     
