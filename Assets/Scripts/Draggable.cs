b205bef7a0fe7914791fe61aafea2cffc26aea38
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2020-11-20 20:02:37 -0500

    Fixed Drag and Drop positioning (I hope)

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -10,37 +10,43 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     [SerializeField] private Canvas canvas;
     [SerializeField] private Vector3 originalPos;
     private CanvasGroup canvasGroup;
+    [SerializeField] private GameObject originalHierarchy;
+    private GameObject draggingHierarchy;
 
     void Start()
     {
         rectTransformObj = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        draggingHierarchy = GameObject.Find("DraggingHierarchy");
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("onpointerdown");
+        //Debug.Log("onpointerdown");
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("onbegindrag");
-        canvasGroup.alpha = .8f;
+        //Debug.Log("onbegindrag");
+        //canvasGroup.alpha = .8f;
         canvasGroup.blocksRaycasts = false;
+        transform.SetParent(draggingHierarchy.transform);
     }
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("ondrag");
+        //Debug.Log("ondrag");
         //Debug.Log(rectTransformObj.anchoredPosition);
         rectTransformObj.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("onenddrag");
-        canvasGroup.alpha = 1f;
+        //Debug.Log("onenddrag");
+        //canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
-        Debug.Log(originalPos);
-        transform.position = originalPos;
+        //Debug.Log(originalPos);
+        rectTransformObj.anchoredPosition = originalPos;
+        //Debug.Log(transform.position);
+        transform.SetParent(originalHierarchy.transform);
     }
 
 }
