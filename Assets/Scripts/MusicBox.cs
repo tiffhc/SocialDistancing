dd08b212a1101580056bc8d3e8f227e9f4da34c9
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-11-21 21:13:30 -0500

    added new script and stuff for music

---
+++ Assets/Scripts/MusicBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBox : MonoBehaviour
+{
+    AudioSource[] audios; //[0] is for themes, [1] is for sfx
+    public AudioSource sfx; 
+   // public AudioClip mainTheme;
+    public AudioClip goodSound;
+    public AudioClip badSound; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audios = this.GetComponents<AudioSource>();
+        audios[0].loop = true;
+        audios[0].Play();
+        sfx = audios[1]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
